d9ed1bedb3462d19637e28185dbb692a9f7ba09f
Ashank Rajendran <ashankrajendran@gmail.com>
2025-07-06 21:41:26 -0400

    Initial commit

---
+++ Vulkan_Start_1/shaders_compute/compile.bat
@@ -0,0 +1,4 @@
+C:/VulkanSDK/1.4.313.1/Bin/glslc.exe shader_compute.comp -o comp.spv
+C:/VulkanSDK/1.4.313.1/Bin/glslc.exe shader_compute.vert -o vert.spv
+C:/VulkanSDK/1.4.313.1/Bin/glslc.exe shader_compute.frag -o frag.spv
+pause
