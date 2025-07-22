4a9ecdbc27903da556c78d375bba5edfdb4f9e37
Ashank Rajendran <ashankrajendran@gmail.com>
2025-07-22 03:03:34 -0400

    Gerstner waves v1

--- Vulkan_Start_1/shaders_compute/compile.bat
+++ Vulkan_Start_1/shaders_compute/compile.bat
@@ -1,5 +1,6 @@
 C:/VulkanSDK/1.4.313.1/Bin/glslc.exe shader_compute.comp -o comp.spv
 C:/VulkanSDK/1.4.313.1/Bin/glslc.exe shader_compute_boid.comp -o comp_boid.spv
+C:/VulkanSDK/1.4.313.1/Bin/glslc.exe shader_compute_gerstner.comp -o comp_gerstner.spv
 C:/VulkanSDK/1.4.313.1/Bin/glslc.exe shader_compute.vert -o vert.spv
 C:/VulkanSDK/1.4.313.1/Bin/glslc.exe shader_compute.frag -o frag.spv
 pause
